a03fb846e720ade4549c1786757b32031238b11f
othmar52 <othmar52@users.noreply.github.com>
2021-12-12 14:38:56 +0100

    [CLEANUP] removed potentiometer and LEDs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# eurorack-gatemod
-Arduino based eurorack gate modifier
+# eurorack gate nudger
+
+arduino based eurorack module to add or remove single gate pulses  
+very useful in combination with the TuringMachine (https://github.com/TomWhitwell/TuringMachine)  
+this module gives us the possibility to nudge a locked sequence left and right on the "timeline" by adding or supressing sigle gate pulses  
+further it is possible to increase the gate length with a potentiometer  
--- cv-gatemod.ino
+++ cv-gatemod.ino
@@ -1,10 +1,17 @@
  
 
+// arduino based eurorack module to add or remove single gate pulses
+// very useful in combination with the TuringMachine (https://github.com/TomWhitwell/TuringMachine)
+// this module gives us the possibility to nudge a locked sequence left and right on the timeline
+// by adding or supressing sigle gate pulses
+// further it is possible to increase the gate length with a potentiometer
 
+// thanks to https://github.com/TomWhitwell/TuringMachine/issues/166
+// thanks to https://fuzzysynth.blogspot.com/2016/07/interfacing-modular-and-microcontrollers.html
 // thanks to https://github.com/joeSeggiola/arduino-eurorack-projects/blob/master/clock-divider/clock-divider.ino
 
+// TODO: documentation & circuit schematics
 // transistor S8050  https://www.el-component.com/images/bipolar-transistor/s8050-pinout.jpg
-
 
 // blue led circuit 3V3:
 //   short leg (-) -> GND
@@ -17,52 +24,34 @@
 // ===========================================================================
 
 
-const int CLOCK_INPUT_PIN = 2; // Input signal pin, must be usable for interrupts
-const int GATE_OUTPUT_PIN = 3; // Gate output
-const int LED_SUPRESS_GATE_PIN = 5;  // indicates a suppressed gate, must support PWM
-const int LED_ADD_GATE_PIN = 6;      // indicates an added gate, must support PWM
+const int CLOCK_INPUT_PIN = 2;  // Input signal pin, must be usable for interrupts
+const int GATE_OUTPUT_PIN = 3;  // Gate output
 
-const int NOTCH_AHEAD_PIN = 4;
-const int NOTCH_BEHIND_PIN = 7;
+const int NOTCH_AHEAD_PIN = 4;  // pushbutton
+const int NOTCH_BEHIND_PIN = 7; // pushbutton
 
-const int LED_GATE_IN_PIN = 8;
-const int LED_GATE_OUT_PIN = 9;
+const int LED_GATE_IN_PIN = 8;  // led indicating incoming clock pulses
+const int LED_GATE_OUT_PIN = 9; // led indicating outgoing clock pulses
 
-const int GATE_LENGTH_POT_PIN = A3;
-const int GATE_MOD_POT_PIN = A2;
-
+const int GATE_LENGTH_POT_PIN = A3; // potentiometer to increase gate length
 const int potentiometerMaxValue = 680; // maximum retrieved value of analogRead() with a 10K trimpot
 
 // ===========================================================================
 
 
-bool ledAddGateGlowing = false;
-bool ledSupressGateGlowing = false;
-
 unsigned long currentMilliSecond = 0;
 
 bool incomingGateState = LOW;
 unsigned long incomingGateLastOpen = 0;
-unsigned long incomingGateLastClose = 0;
 unsigned long incomingGateInterval = 0;
 unsigned long incomingGateLength = 0;
 
-
 bool outgoingGateState = LOW;
 unsigned long outgoingGateLastOpen = 0;
-unsigned long outgoingGateLastClose = 0;
-unsigned long outgoingGateInterval = 0;
-unsigned long outgoingGateLength = 0;
-
 
 bool notchAheadButtonState = LOW;
 bool notchBehindButtonState = LOW;
 
-
-unsigned int maxGateModPerMinute = 100; // maximum of added or supressed gates per minute
-unsigned long lastAdditionalGateMilliSecond = 0;
-unsigned long lastSupressedGateMilliSecond = 0;
-bool nowAddingGate = false;
 bool nowSupressingGate = false;
 
 unsigned int addGatesQueue = 0;
@@ -71,171 +60,65 @@ unsigned int supressGatesQueue = 0;
 const int minimumGapLength = 2; // milliseconds between LOW and HIGH; so maximum gate length will be gate interval minus this value [ms]
 
 void gateOutHigh() {
-  digitalWrite(GATE_OUTPUT_PIN, LOW); // inverted
-  digitalWrite(LED_GATE_OUT_PIN, HIGH);
+  if (outgoingGateState == HIGH) {
+    return;
+  }
   outgoingGateState = HIGH;
   outgoingGateLastOpen = currentMilliSecond;
-  //Serial.println("gateOutHigh");
-  //Serial.println(analogRead(GATE_MOD_POT_PIN));
-  //Serial.print("add gates per second: ");
-  //Serial.println(getAddGateInterval());
-  //Serial.print("supress gates per second: ");
-  //Serial.println(getSupressGateInterval());
+  digitalWrite(GATE_OUTPUT_PIN, LOW);   // inverted
+  digitalWrite(LED_GATE_OUT_PIN, HIGH);
   
 }
 
 void gateOutLow() {
+  if (outgoingGateState == LOW) {
+    return;
+  }
+  outgoingGateState = LOW;
   digitalWrite(GATE_OUTPUT_PIN, HIGH); // inverted
   digitalWrite(LED_GATE_OUT_PIN, LOW);
-  outgoingGateState = LOW;
-  outgoingGateLastClose = currentMilliSecond;
-  //Serial.println("gateOutLow");
-  //Serial.println(analogRead(GATE_LENGTH_POT_PIN));
 }
 
 void checkGateOutHigh() {
   if (outgoingGateState == HIGH) {
     return;  
   }
-  /*
-  if(getSupressGateInterval() > 0) {
-    if(currentMilliSecond - lastSupressedGateMilliSecond > getSupressGateInterval()) {
-      lastSupressedGateMilliSecond = currentMilliSecond;
-      nowSupressingGate = true;
-      ////digitalWrite(LED_SUPRESS_GATE_PIN, HIGH);
-      gateSupressLedIndicator(HIGH);
-      gateAddLedIndicator(LOW);
-      Serial.print("suppressing gate...  interval is ");
-      Serial.println(getSupressGateInterval());
-      return;
-    }
-  }
-  */
-  
-
   if (supressGatesQueue > 0) {
-      Serial.println("suppressing gate");
-      nowSupressingGate = true;
-      supressGatesQueue--;
+    nowSupressingGate = true;
+    supressGatesQueue--;
   }
   
   if (nowSupressingGate == true) {
     return;
   }
-  /**
-  if(getAddGateInterval() > 0) {
-    
-    
-    //if (ledAddGateGlowing == false) {
-    //  digitalWrite(LED_ADD_GATE_ACTIVE_PIN, HIGH);
-    //  ledAddGateGlowing = true;
-    //  analogWrite( LED_ADD_GATE_PIN, 30);
-    //}
-    if(currentMilliSecond - lastAdditionalGateMilliSecond > getAddGateInterval()) {
-      lastAdditionalGateMilliSecond = currentMilliSecond;
-      nowAddingGate = true;
-      ////digitalWrite(LED_ADD_GATE_PIN, HIGH);
-      gateAddLedIndicator(HIGH);
-      gateSupressLedIndicator(LOW);
-      Serial.print("adding gate...  interval is ");
-      Serial.println(getAddGateInterval());
-      gateOutHigh();
-      gateOutLow();
-    }
-  }
-
-  if (nowAddingGate == true && currentMilliSecond - lastAdditionalGateMilliSecond > 120) {
-      nowAddingGate = false;
-      ////digitalWrite(LED_ADD_GATE_PIN, LOW);
-      gateAddLedIndicator(LOW);
-  }
-  */
-
+  
   if (addGatesQueue > 0) {
-      Serial.print("adding gate...  interval is ");
-      Serial.println(getAddGateInterval());
-      gateOutHigh();
-      gateOutLow();
-      addGatesQueue--;
+    gateOutHigh();
+    gateOutLow();
+    addGatesQueue--;
   }
 
-  if(incomingGateState != outgoingGateState) {
+  if (outgoingGateState == LOW && incomingGateState != outgoingGateState) {    
     gateOutHigh();
   }
 }
 
 void loopPushButtons() {
-  if(digitalRead(NOTCH_AHEAD_PIN) != notchAheadButtonState) {
+  if (digitalRead(NOTCH_AHEAD_PIN) != notchAheadButtonState) {
     notchAheadButtonState = digitalRead(NOTCH_AHEAD_PIN);
-    if(notchAheadButtonState == HIGH) {
-      addGatesQueue++;  
+    if (notchAheadButtonState == HIGH) {
+      // shouldn't it be the other way round? anyway...
+      supressGatesQueue++;
     }
-    Serial.print("notch ahead "); Serial.println(notchAheadButtonState);
   }
-  if(digitalRead(NOTCH_BEHIND_PIN) != notchBehindButtonState) {
+  if (digitalRead(NOTCH_BEHIND_PIN) != notchBehindButtonState) {
     notchBehindButtonState = digitalRead(NOTCH_BEHIND_PIN);
-    if(notchBehindButtonState == HIGH) {
-      supressGatesQueue++;  
+    if (notchBehindButtonState == HIGH) {
+      // shouldn't it be the other way round? anyway...
+      addGatesQueue++;
     }
-    Serial.print("notch behind "); Serial.println(notchBehindButtonState);
+    //Serial.print("notch behind "); Serial.println(notchBehindButtonState);
   }
-}
-
-void gateSupressLedIndicator(bool highOrLow) {
-  if (highOrLow == HIGH) {
-    digitalWrite(LED_SUPRESS_GATE_PIN, HIGH);
-    analogWrite(LED_SUPRESS_GATE_PIN, 150);
-    return;
-  }
-
-  if(getSupressGateInterval() > 0) {
-    analogWrite(LED_SUPRESS_GATE_PIN, 10);
-    return;
-  }
-  digitalWrite(LED_SUPRESS_GATE_PIN, LOW);
-}
-
-void gateAddLedIndicator(bool highOrLow) {
-  if (highOrLow == HIGH) {
-    digitalWrite(LED_ADD_GATE_PIN, HIGH);
-    analogWrite(LED_ADD_GATE_PIN, 150);
-    return;
-  }
-
-  if(getAddGateInterval() > 0) {
-    analogWrite(LED_ADD_GATE_PIN, 10);
-    return;
-  }
-  digitalWrite(LED_ADD_GATE_PIN, LOW);
-}
-
-
-// returns value between zero and maxGateModPerMinute
-// in case the potentiometer is between 60% and 100%;
-unsigned int getAddGateInterval() {
-  int gatesPerMinute = 0;
-  if (analogRead(GATE_MOD_POT_PIN) < potentiometerMaxValue*0.6) {
-    return gatesPerMinute;
-  }
-  gatesPerMinute = map(analogRead(GATE_MOD_POT_PIN), potentiometerMaxValue*0.6, potentiometerMaxValue, 1, maxGateModPerMinute);
-  if(gatesPerMinute < 1) {
-    return 0;  
-  }
-  return 60000/gatesPerMinute;
-}
-
-// returns value between zero and maxGateModPerMinute
-// in case the potentiometer is between 0% and 40%;
-unsigned int getSupressGateInterval() {
-  int gatesPerMinute = 0;
-  if (analogRead(GATE_MOD_POT_PIN) > potentiometerMaxValue*0.4) {
-    return gatesPerMinute;
-  }
-  gatesPerMinute = map(analogRead(GATE_MOD_POT_PIN), 0, potentiometerMaxValue*0.4, maxGateModPerMinute, 1);
-  if(gatesPerMinute < 1) {
-    return 0;  
-  }
-  return 60000/gatesPerMinute;
 }
 
 
@@ -244,18 +127,35 @@ void checkGateOutLow() {
     return;  
   }
 
-  if(analogRead(GATE_LENGTH_POT_PIN) == 0 && incomingGateState != outgoingGateState) {
-    gateOutLow();
+  // check if we have to modify gate length
+  int valueGateLengthPot = analogRead(GATE_LENGTH_POT_PIN);
+
+  if (valueGateLengthPot < 2) {
+    // no gate length modification - just copy the incoming gates
+    if (incomingGateState != outgoingGateState) {
+      gateOutLow();
+    }
     return;
   }
-  // the current gate length is bigger than incoming gate length map(value, fromLow, fromHigh, toLow, toHigh)
-  unsigned int requestedGateLength = map(analogRead(GATE_LENGTH_POT_PIN), 0, potentiometerMaxValue, incomingGateLength, incomingGateInterval-minimumGapLength);
+
+  // we do have a gate length modification
+  // the current gate length is bigger than incoming gate length
+  int requestedGateLength = map(
+    valueGateLengthPot,                    // value
+    0,                                     // fromLow
+    potentiometerMaxValue,                 // fromHigh
+    incomingGateLength,                    // toLow
+    incomingGateInterval-minimumGapLength  // toHigh
+  );
+
+  // just to make sure we have no quirks
+  if (requestedGateLength < incomingGateLength) {
+    requestedGateLength = incomingGateLength;
+  }
   if (requestedGateLength < minimumGapLength) {
-    Serial.print("DANGER: ");
-    Serial.println(requestedGateLength);
     requestedGateLength = minimumGapLength;
   }
-  if(currentMilliSecond >= outgoingGateLastOpen + requestedGateLength) {
+  if (currentMilliSecond >= outgoingGateLastOpen + requestedGateLength) {
     gateOutLow();
   }
   
@@ -271,12 +171,9 @@ void handleIncomingGateChangeToHigh() {
 
 void handleIncomingGateChangeToLow() {
   incomingGateLength = currentMilliSecond - incomingGateLastOpen;
-  incomingGateLastClose = currentMilliSecond;
   digitalWrite(LED_GATE_IN_PIN, LOW);
   incomingGateState = LOW;
   nowSupressingGate = false;
-  //digitalWrite(LED_SUPRESS_GATE_PIN, LOW);
-  gateSupressLedIndicator(LOW);
 }
 
 void setup()
@@ -293,32 +190,24 @@ void loop()
   loopPushButtons();
 }
 
-
-
 void setupCvClocksAndLeds() {
   // Interrupts
   pinMode(CLOCK_INPUT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(CLOCK_INPUT_PIN), isrClock, CHANGE);
+  
   pinMode(NOTCH_AHEAD_PIN, INPUT_PULLUP);
   pinMode(NOTCH_BEHIND_PIN, INPUT_PULLUP);
 
   pinMode(GATE_OUTPUT_PIN, OUTPUT);
   pinMode(LED_GATE_IN_PIN, OUTPUT);
   pinMode(LED_GATE_OUT_PIN, OUTPUT);
-  pinMode(LED_SUPRESS_GATE_PIN, OUTPUT);
-  pinMode(LED_ADD_GATE_PIN, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(CLOCK_INPUT_PIN), isrClock, CHANGE);
 }
 
 
 void isrClock() {
-  if(digitalRead(CLOCK_INPUT_PIN) == HIGH) {
+  if (digitalRead(CLOCK_INPUT_PIN) == HIGH) {
     handleIncomingGateChangeToLow();
-  } else {
-    handleIncomingGateChangeToHigh();
+    return;
   }
-  //Serial.print("interval ");
-  //Serial.println( incomingGateInterval);
-  //Serial.print("length ");
-  //Serial.println(incomingGateLength);
-  
+  handleIncomingGateChangeToHigh();
 }

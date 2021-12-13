eb67f43d51d61ffb5eee3cb8bd30490a54c2ca79
othmar52 <othmar52@users.noreply.github.com>
2021-12-13 08:40:02 +0100

    [DOCUMENTATION] add circuit schematics & parts list

--- README.md
+++ README.md
@@ -2,5 +2,21 @@
 
 arduino based eurorack module to add or remove single gate pulses  
 very useful in combination with the TuringMachine (https://github.com/TomWhitwell/TuringMachine)  
-this module gives us the possibility to nudge a locked sequence left and right on the "timeline" by adding or supressing sigle gate pulses  
+this module gives us the possibility to nudge a locked sequence left and right on the "timeline" by adding or supressing single gate pulses  
 further it is possible to increase the gate length with a potentiometer  
+
+## parts
+ - Arduino
+ - 2 x 3.5mm TRS female jacks (CV clock in/out)
+ - 2 x NPN Transistor N3904 (CV in/out circuit)
+ - 6 x Resistor 10K (CV in/out circuit)
+ - 1 x Resistor 1K (CV out circuit)
+ - 1 x Diode 1N4148 (CV in circuit)
+ - 2 x LED blue (in/out gate indicator)
+ - 2 x Resistor 4.7K (for 2 LEDs)
+ - 1 x Trimpot 10K (for gate length)
+ - 2 x Pushbuttons (for nudging)
+ - 1 x Box Header 16 pin (for connection to power BUS)
+## circuit
+[![Eurorack DIY gate nudger](media/arduino-eurorack-diy-gate-nudger-circuit-schematics.png "Eurorack DIY gate nudger")](https://github.com/othmar52/eurorack-gatenudger/blob/master/media/arduino-eurorack-diy-gate-nudger-circuit-schematics.png?raw=true)  
+
--- cv-gatemod.ino
+++ cv-gatemod.ino
@@ -1,5 +1,6 @@
  
 
+// https://github.com/othmar52/eurorack-gatenudger
 // arduino based eurorack module to add or remove single gate pulses
 // very useful in combination with the TuringMachine (https://github.com/TomWhitwell/TuringMachine)
 // this module gives us the possibility to nudge a locked sequence left and right on the timeline
@@ -27,14 +28,14 @@
 const int CLOCK_INPUT_PIN = 2;  // Input signal pin, must be usable for interrupts
 const int GATE_OUTPUT_PIN = 3;  // Gate output
 
-const int NOTCH_AHEAD_PIN = 4;  // pushbutton
-const int NOTCH_BEHIND_PIN = 7; // pushbutton
+const int NUDGE_AHEAD_PIN = 4;  // pushbutton
+const int NUDGE_BEHIND_PIN = 7; // pushbutton
 
 const int LED_GATE_IN_PIN = 8;  // led indicating incoming clock pulses
 const int LED_GATE_OUT_PIN = 9; // led indicating outgoing clock pulses
 
 const int GATE_LENGTH_POT_PIN = A3; // potentiometer to increase gate length
-const int potentiometerMaxValue = 680; // maximum retrieved value of analogRead() with a 10K trimpot
+const int potentiometerMaxValue = 680; // maximum retrieved value of analogRead() with the 10K trimpot i am using. shouldn't this be 1024?
 
 // ===========================================================================
 
@@ -81,14 +82,12 @@ void gateOutLow() {
 
 void checkGateOutHigh() {
   if (outgoingGateState == HIGH) {
+    // already high - no need for further checks
     return;  
   }
   if (supressGatesQueue > 0) {
     nowSupressingGate = true;
     supressGatesQueue--;
-  }
-  
-  if (nowSupressingGate == true) {
     return;
   }
   
@@ -104,15 +103,15 @@ void checkGateOutHigh() {
 }
 
 void loopPushButtons() {
-  if (digitalRead(NOTCH_AHEAD_PIN) != notchAheadButtonState) {
-    notchAheadButtonState = digitalRead(NOTCH_AHEAD_PIN);
+  if (digitalRead(NUDGE_AHEAD_PIN) != notchAheadButtonState) {
+    notchAheadButtonState = digitalRead(NUDGE_AHEAD_PIN);
     if (notchAheadButtonState == HIGH) {
       // shouldn't it be the other way round? anyway...
       supressGatesQueue++;
     }
   }
-  if (digitalRead(NOTCH_BEHIND_PIN) != notchBehindButtonState) {
-    notchBehindButtonState = digitalRead(NOTCH_BEHIND_PIN);
+  if (digitalRead(NUDGE_BEHIND_PIN) != notchBehindButtonState) {
+    notchBehindButtonState = digitalRead(NUDGE_BEHIND_PIN);
     if (notchBehindButtonState == HIGH) {
       // shouldn't it be the other way round? anyway...
       addGatesQueue++;
@@ -124,6 +123,7 @@ void loopPushButtons() {
 
 void checkGateOutLow() {
   if (outgoingGateState == LOW) {
+    // already low - no need for further checks
     return;  
   }
 
@@ -131,23 +131,37 @@ void checkGateOutLow() {
   int valueGateLengthPot = analogRead(GATE_LENGTH_POT_PIN);
 
   if (valueGateLengthPot < 2) {
-    // no gate length modification - just copy the incoming gates
+    // potentiometer is fully turned counter clockwise
+    // no gate length modification - just pass through the incoming gate pulses
     if (incomingGateState != outgoingGateState) {
       gateOutLow();
     }
     return;
   }
 
-  // we do have a gate length modification
-  // the current gate length is bigger than incoming gate length
+  // we do have an increased gate length due to potentiometer value
+  int modifiedGateLength = calculateModifiedGateLengthMilliSeconds(
+    valueGateLengthPot
+  );
+
+  // check if it's time to close the open gate
+  if (currentMilliSecond < outgoingGateLastOpen + modifiedGateLength) {
+    return;
+  }
+  gateOutLow();
+}
+
+// the current gate length is longer than incoming gate length
+  // with the potentiometer fully turned clockwise the gatelength will be as long as the incoming gate interval - 2 milliseconds
+int calculateModifiedGateLengthMilliSeconds(int potentiometerValue) {
   int requestedGateLength = map(
-    valueGateLengthPot,                    // value
+    potentiometerValue,                    // value
     0,                                     // fromLow
     potentiometerMaxValue,                 // fromHigh
     incomingGateLength,                    // toLow
     incomingGateInterval-minimumGapLength  // toHigh
   );
-
+  
   // just to make sure we have no quirks
   if (requestedGateLength < incomingGateLength) {
     requestedGateLength = incomingGateLength;
@@ -155,11 +169,7 @@ void checkGateOutLow() {
   if (requestedGateLength < minimumGapLength) {
     requestedGateLength = minimumGapLength;
   }
-  if (currentMilliSecond >= outgoingGateLastOpen + requestedGateLength) {
-    gateOutLow();
-  }
-  
-  
+  return requestedGateLength;
 }
 
 void handleIncomingGateChangeToHigh() {
@@ -195,8 +205,8 @@ void setupCvClocksAndLeds() {
   pinMode(CLOCK_INPUT_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(CLOCK_INPUT_PIN), isrClock, CHANGE);
   
-  pinMode(NOTCH_AHEAD_PIN, INPUT_PULLUP);
-  pinMode(NOTCH_BEHIND_PIN, INPUT_PULLUP);
+  pinMode(NUDGE_AHEAD_PIN, INPUT_PULLUP);
+  pinMode(NUDGE_BEHIND_PIN, INPUT_PULLUP);
 
   pinMode(GATE_OUTPUT_PIN, OUTPUT);
   pinMode(LED_GATE_IN_PIN, OUTPUT);
